ac6205a886fb85835702bec77d6479baaa57ef26
jahangiranvarxujaev <jahangiranvarxujaev@gmail.com>
2022-07-13 16:56:08 +0500

    adding h/w files

--- 04-dict/dz - 9.py
+++ 04-dict/dz - 9.py
@@ -1,11 +1,20 @@
 string = input()
 initial_list = string.split(' ')
 print(initial_list)
-common_list = []
+common_list = {}
 final_list = []
 
 for word in initial_list:
-    common_list.append(word)
-    if common_list.count(word) <= 1:
-        final_list.append(word)
-print(final_list)
+    common_list[word] = common_list.get(word, 0) + 1
+
+min_value = 99999999
+final_key = ''
+for key, value in common_list.items():
+    if value < min_value:
+        min_value = value
+        final_key = key
+
+    # else:
+    #     final_key =
+
+print(final_key)
